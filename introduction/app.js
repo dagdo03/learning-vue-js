2893b95341c93baf11390611239277a6f4148c11
dagdo03 <ihsan.widagdo@gmail.com>
2023-10-31 20:59:09 +0700

    feat: proses sampe video 5 click event and method

---
+++ introduction/app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title: 'Pemrograman Web',
+            author: 'Ihsan Widagdo',
+            age: '20',
+        }
+    }
+})
+app.mount('#app')
